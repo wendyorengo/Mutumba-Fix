042afcc8f10cbcafb69948ec7c3c7a8f1f808162
wilsenock220 <wilsenock220@gmail.com>
2019-12-03 16:33:30 +0300

    add a register function and install bootstrap to project

--- app/views.py
+++ app/views.py
@@ -2,13 +2,23 @@ from flask import render_template
 from app import app
 
 # Views
-@app.route('/')
+@app.route('/login')
 def login():
 
     '''
-    View root page function that returns the index page and its data
+    View root page function that returns the login and its data
     '''
     return render_template('login.html')
+
+@app.route('/register')
+def register():
+
+    '''
+    View root page function that returns the register page and its data
+    '''
+    return render_template('register.html')
+
+
 
 
 
