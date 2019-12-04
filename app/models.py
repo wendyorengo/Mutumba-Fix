b51f50bcd5be11c052d4892eb79b93d365e74c7b
wilsenock220 <wilsenock220@gmail.com>
2019-12-04 11:24:10 +0300

    create password functions that verifys the input passwords

--- app/models.py
+++ app/models.py
@@ -20,4 +20,18 @@ class User(UserMixin, db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
-    blog = db.relationship('Blog',backref = 'user',lazy = "dynamic")
+    post = db.relationship('Post',backref = 'user',lazy = "dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password) 
+       
+    def __repr__(self):
+        return f'User {self.username}'
