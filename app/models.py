b75fe0ad3e34ac7ba03eb29cf7566d808fbb4bcc
wilsenock220 <wilsenock220@gmail.com>
2019-12-04 11:21:26 +0300

    add a class user with database columns and make a db importation

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from . import db
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String(255))
+    blog = db.relationship('Blog',backref = 'user',lazy = "dynamic")
