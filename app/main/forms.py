a4ba3e6c016b08517b5f0fd8bc5e93839582ac32
wilsenock220 <wilsenock220@gmail.com>
2019-12-04 10:44:47 +0300

    add a login class with required fields and importation

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
-from wtfforms import StringField
-
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
-    
     submit = SubmitField('Sign Up')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    submit = SubmitField('Sign Up')
+
+
+
