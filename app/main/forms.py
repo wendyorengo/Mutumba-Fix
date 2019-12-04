704b07c6874fcd956a04a44e52e1dd96fa1fc4ec
wilsenock220 <wilsenock220@gmail.com>
2019-12-04 10:33:59 +0300

    create a class registrationform and import stringfield

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtfforms import StringField
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators = [Required()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    
+    submit = SubmitField('Sign Up')
---
+++ app/main/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+# Views
+@app.route('/login')
+def login():
+
+    '''
+    View root page function that returns the login and its data
+    '''
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+
+    '''
+    View root page function that returns the register page and its data
+    '''
+    return render_template('register.html')
+
