a236ec3321962cf3ead85456aeec07774c651b84
wilsenock220 <wilsenock220@gmail.com>
2019-12-05 10:45:44 +0300

    fix errors

--- config.py
+++ config.py
@@ -3,9 +3,5 @@
 
 
 
-    # email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
+    
