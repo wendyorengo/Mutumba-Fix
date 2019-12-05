01ad00483d6d76b43885dcb553cbc172d23f0957
margaret254 <maggiemwas91@gmail.com>
2019-12-05 22:42:23 +0300

    Updated post.html

--- app/main/views.py
+++ app/main/views.py
@@ -51,7 +51,7 @@ def all():
     title = 'Welcome to Mitumba Fix Application'
 
     
-    profile= Post.get_all_profiles() 
+    posts= Post.get_all_posts() 
     
 
     return render_template('profile.html', title = title, profiles=profiles)
